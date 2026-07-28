8ddc90f6f02388636b03d7ad8ed41ccb56fbaf34
MagnusUnger <262898955+MagnusUnger@users.noreply.github.com>
2026-07-28 22:42:37 +0200

    bublee_sort function and lil refactoring

--- starwars.py
+++ starwars.py
@@ -2,11 +2,19 @@
 
 array = [1,5,4,8,2,3,9,7,6]
 
-for i in range(len(array)):
-    for j in range(len(array)-1):
-        if array[j] > array[j+1]:
-            temp        = array[j]
-            array[j]    = array[j + 1]
-            array[j + 1]= temp
+def bublee_sort(array: list) ->list:
+    for i in range(len(array)):
+        was_swapped = False
 
-print(array)
+        for j in range(len(array)-1):
+            if array[j] > array[j+1]:
+                array[j], array[j + 1] = array[j +1], array[j]
+                was_swapped = True
+
+        #if no swap happened in iteration -> already sorted (checks for every iteration)
+        if was_swapped == False:
+            break
+
+    return array
+
+print(bublee_sort(array))
