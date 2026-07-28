2cb7bd3741f1788ab875834248ff32049fe8ab72
MagnusUnger <262898955+MagnusUnger@users.noreply.github.com>
2026-07-28 22:42:38 +0200

    saving the helpful python doc link

--- pythondoc.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-
-webbrowser.open_new_tab("https://docs.python.org/3/library/index.html")
-
-#i would really like to use that library one day! It looks interesting
-
-#i would really like to use that library one day! It looks interesting!!
